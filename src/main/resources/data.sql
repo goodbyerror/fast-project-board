f7132cb974b348867625a484386fe48283e64d24
goodbyerror <goodbyerror@gmail.com>
2022-07-22 16:50:39 +0900

    #10 - 테스트 데이터를 data.sql로 추가
    데이터는 mokaroo로 생성
    https://mockaroo.com/

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+-- 123 게시글
+insert into article (title, content, hashtag, created_by, modified_by, created_at, modified_at) values
+('Ut tellus.', 'Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.
+Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.', null, 'Wilmer', 'Ingra', '2021-07-19 14:18:17', '2022-01-23 17:29:54')
+;
