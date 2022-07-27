ebd99bac8d2c0b97bd256115699660a4a4480c88
goodbyerror <goodbyerror@gmail.com>
2022-07-27 12:38:05 +0900

    #1 - 데이터 조회 안되던 거 수정함

--- src/main/java/com/fastcampus/projectboard/domain/AuditingField.java
+++ src/main/java/com/fastcampus/projectboard/domain/AuditingField.java
@@ -6,6 +6,7 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.Column;
@@ -15,7 +16,7 @@ import java.time.LocalDateTime;
 
 @Getter
 @ToString
-@EntityListeners(AuditingField.class)
+@EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 public class AuditingField {
 
