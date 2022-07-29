fefdd0dd0b4c7d2c52460137bf7ba5d3f1e0e070
goodbyerror <goodbyerror@gmail.com>
2022-07-29 13:45:22 +0900

    #17 - 뷰에 디자인 코드 작성하기

--- src/test/java/com/fastcampus/projectboard/controller/AuthControllerTest.java
+++ src/test/java/com/fastcampus/projectboard/controller/AuthControllerTest.java
@@ -1,20 +1,14 @@
 package com.fastcampus.projectboard.controller;
 
-import com.fastcampus.projectboard.config.SecurityConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.context.annotation.Import;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @DisplayName("view auth controller")
-@Import(SecurityConfig.class)
 @WebMvcTest
 public class AuthControllerTest {
 
@@ -26,9 +20,7 @@ public class AuthControllerTest {
 
     @DisplayName("auth test")
     @Test
-    void given_when_then() throws Exception {
+    void given_when_then()) {
         mvc.perform(get("/login"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
     }
 }
