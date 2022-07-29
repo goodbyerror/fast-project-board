21cf061960c5f7fa420800e7ffdbe22b523115ae
goodbyerror <goodbyerror@gmail.com>
2022-07-29 11:27:20 +0900

    #17 - 인증 테스트 작성

--- src/test/java/com/fastcampus/projectboard/controller/AuthControllerTest.java
+++ src/test/java/com/fastcampus/projectboard/controller/AuthControllerTest.java
@@ -1,14 +1,20 @@
 package com.fastcampus.projectboard.controller;
 
+import com.fastcampus.projectboard.config.SecurityConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @DisplayName("view auth controller")
+@Import(SecurityConfig.class)
 @WebMvcTest
 public class AuthControllerTest {
 
@@ -20,7 +26,9 @@ public class AuthControllerTest {
 
     @DisplayName("auth test")
     @Test
-    void given_when_then()) {
+    void given_when_then() throws Exception {
         mvc.perform(get("/login"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
     }
 }
